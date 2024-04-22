b65b97986b775d949a00292d84bbc951b5e8c5e3
Akshay Ballal <arballal95@gmail.com>
2024-04-23 00:42:45 +0200

    Add url dependency and fix typo in WebsiteProcessor

--- examples/web_embed.rs
+++ examples/web_embed.rs
@@ -3,9 +3,11 @@ use candle_core::Tensor;
 
 #[tokio::main]
 async fn main() {
-    let url = "https://en.wikipedia.org/wiki/Long_short-term_memory";
 
-    let website_processor = website_processor::WebsiteProcesor;
+    let start_time = std::time::Instant::now();
+    let url = "https://www.scrapingbee.com/blog/web-scraping-rust/";
+
+    let website_processor = website_processor::WebsiteProcessor;
     let webpage = website_processor.process_website(url).await.unwrap();
     let embeder = embed_anything::embedding_model::bert::BertEmbeder::default();
     let embed_data = webpage.embed_webpage(&embeder).unwrap();
@@ -17,7 +19,7 @@ async fn main() {
         &candle_core::Device::Cpu,
     ).unwrap();
 
-    let query = vec!["how to use lstm for nlp".to_string()];
+    let query = vec!["Rust for web scraping".to_string()];
     let query_embedding: Vec<f32> = embeder.embed(&query, None).unwrap().iter().map(|data| data.embedding.clone()).flatten().collect();
     
     let query_embedding_tensor = Tensor::from_vec(
@@ -40,5 +42,6 @@ async fn main() {
     let data = &embed_data[max_similarity_index];
 
     println!("{:?}", data);
+    println!("Time taken: {:?}", start_time.elapsed());
     
 }
