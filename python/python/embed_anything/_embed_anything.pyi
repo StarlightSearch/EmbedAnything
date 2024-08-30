ec0914eb2be1268bb9884938b52d860de6dda1d5
Akshay Ballal <arballal95@gmail.com>
2024-08-30 18:10:10 +0530

    chore: Update embedding model configurations and examples
    Former-commit-id: efbbe4584f4b460d63eaa935f8a1dc4d1d533e5d [formerly a74e99a0d1da49190d63ff4776cd6403e1647e42] [formerly cf0123a140533fc8e565600da5ae12a33a8addd5 [formerly 881905fe2efa6da1d4df5d310c86d1dbdf8f3d5c]]
    Former-commit-id: cf0123a140533fc8e565600da5ae12a33a8addd5
    Former-commit-id: b9eccafdbac7aee089c7ec9bde7c28b8314b56b4

--- python/python/embed_anything/_embed_anything.pyi
+++ python/python/embed_anything/_embed_anything.pyi
@@ -14,46 +14,15 @@ class Adapter(ABC):
     def upsert(self, data: List[Dict]): ...
 
 def embed_query(
-    query: list[str], embeder: str, config: EmbedConfig | None = None
+    query: list[str], embeder: EmbeddingModel, config: TextEmbedConfig | None = None
 ) -> list[EmbedData]:
-    """Embeds the given query and returns an EmbedData object.
+    """
+    Embeds the given query and returns a list of EmbedData objects.
 
     Args:
         query: The query to embed.
-        embeder: The name of the embedding model to use. Choose between "OpenAI", "Jina", "Bert"
+        embeder: The embedding model to use.
         config: The configuration for the embedding model.
-
-    Returns:
-        An EmbedData object.
-
-    Example:
-    ```python
-    import embed_anything
-    config = embed_anything.EmbedConfig(
-        bert=embed_anything.BertConfig(model_id="sentence-transformers/allMiniLM-L6-v2"),
-    )
-    data = embed_anything.embed_query(["Hello World", "This is a query"], embeder="Bert, config = config)
-    ```
-    """
-
-def embed_file(
-    file_path: str,
-    embeder: str,
-    config: EmbedConfig | None = None,
-    adapter: Adapter | None = None,
-) -> list[EmbedData]:
-    """Embeds the file at the given path and returns a list of EmbedData
-    objects.
-
-    - Text -> "OpenAI", "Bert"
-    - Image -> "Clip"
-    - Audio -> "Audio"
-
-    Args:
-        file_path: The path to the file to embed.
-        embeder: The name of the embedding model to use.
-        config: The configuration for the embedding model.
-        adapter: The adapter to use for storing the embeddings
 
     Returns:
         A list of EmbedData objects.
@@ -61,31 +30,26 @@ def embed_file(
     Example:
     ```python
     import embed_anything
-    config = embed_anything.EmbedConfig(
-                    openai_config=embed_anything.OpenAIConfig(
-                    model="text-embedding-3-small")
-                    )
-    data = embed_anything.embed_file("test_files/test.pdf",
-                                    embeder="OpenAI,
-                                    config = config)
-    ```
+    model = embed_anything.EmbeddingModel.from_pretrained_local(
+        embed_anything.WhichModel.Bert,
+        model_id="sentence-transformers/all-MiniLM-L6-v2",
+        revision="main",
+    )
     """
 
-def embed_directory(
+def embed_file(
     file_path: str,
-    embeder: str,
-    extensions: list[str],
+    embeder: EmbeddingModel,
     config: TextEmbedConfig | None = None,
     adapter: Adapter | None = None,
 ) -> list[EmbedData]:
-    """Embeds all the files in the given directory and returns a list of
-    EmbedData objects.
+    """
+    Embeds the given file and returns a list of EmbedData objects.
 
     Args:
-        file_path: The path to the directory containing the files to embed.
-        embeder: The name of the embedding model to use. Choose between "OpenAI", "Jina", "Bert"
-        extensions: The list of file extensions to consider for embedding. Choose from ["txt", "pdf", "md"]
-        config:  The configuration for the embedding model.
+        file_path: The path to the file to embed.
+        embeder: The embedding model to use.
+        config: The configuration for the embedding model.
         adapter: The adapter to use for storing the embeddings in a vector database.
 
     Returns:
@@ -94,16 +58,51 @@ def embed_directory(
     Example:
     ```python
     import embed_anything
-    config = embed_anything.EmbedConfig(
-                    bert_config = embed_anything.BertConfig(
-                    model_id="sentence-transformers/allMiniLM-L6-v2")
-                    )
-    data = embed_anything.embed_directory("test_files", embeder="Bert", config=config)
-    ```
+    model = embed_anything.EmbeddingModel.from_pretrained_local(
+        embed_anything.WhichModel.Bert,
+        model_id="sentence-transformers/all-MiniLM-L6-v2",
+        revision="main",
+    )
+    data = embed_anything.embed_file("test_files/test.pdf", embeder=model)
+
+    """
+
+def embed_directory(
+    file_path: str,
+    embeder: EmbeddingModel,
+    extensions: list[str],
+    config: TextEmbedConfig | None = None,
+    adapter: Adapter | None = None,
+) -> list[EmbedData]:
+    """
+    Embeds the files in the given directory and returns a list of EmbedData objects.
+
+    Args:
+        file_path: The path to the directory containing the files to embed.
+        embeder: The embedding model to use.
+        extensions: The list of file extensions to consider for embedding.
+        config: The configuration for the embedding model.
+        adapter: The adapter to use for storing the embeddings in a vector database.
+
+    Returns:
+        A list of EmbedData objects.
+
+    Example:
+    ```python
+    import embed_anything
+    model = embed_anything.EmbeddingModel.from_pretrained_local(
+        embed_anything.WhichModel.Bert,
+        model_id="sentence-transformers/all-MiniLM-L6-v2",
+        revision="main",
+    )
+    data = embed_anything.embed_directory("test_files", embeder=model, extensions=[".pdf"])
     """
 
 def embed_webpage(
-    url: str, embeder: str, config: EmbedConfig | None, adapter: Adapter | None
+    url: str,
+    embeder: EmbeddingModel,
+    config: TextEmbedConfig | None,
+    adapter: Adapter | None,
 ) -> list[EmbedData] | None:
     """Embeds the webpage at the given URL and returns a list of EmbedData
     objects.
@@ -147,61 +146,6 @@ class EmbedData:
     text: str
     metadata: dict[str, str]
 
-class EmbedConfig:
-    """Represents the configuration for the embedding model. Each config object
-    should hold only one of the embedding model. Do not provide multiple.
-
-    Attributes:
-        bert: The configuration for the Bert model.
-        openai: The configuration for the OpenAI model.
-        clip: The configuration for the Clip model.
-    """
-
-    def __init__(
-        self,
-        bert: BertConfig | None = None,
-        jina: JinaConfig | None = None,
-        cloud: CloudConfig | None = None,
-        clip: ClipConfig | None = None,
-        audio_decoder: AudioDecoderConfig | None = None,
-    ):
-        self.bert = bert
-        self.cloud = cloud
-        self.jina = jina
-        self.clip = clip
-        self.audio_decoder = audio_decoder
-    bert: BertConfig | None
-    jina: JinaConfig | None
-    openai: CloudConfig | None
-    clip: ClipConfig | None
-    audio_decoder: AudioDecoderConfig | None
-
-class BertConfig:
-    """Represents the configuration for the Bert model.
-
-    Attributes:
-        model_id: The ID of the Bert model from huggingface.
-        revision: The revision of the Bert model.
-        chunk_size: The chunk size for the Bert model.
-        batch_size: The batch size for processing the embeddings. Default is 32. Based on the memory, you can increase or decrease the batch size.
-    """
-
-    def __init__(
-        self,
-        model_id: str | None = None,
-        revision: str | None = None,
-        chunk_size: int | None = None,
-        batch_size: int | None = None,
-    ):
-        self.model_id = model_id
-        self.revision = revision
-        self.chunk_size = chunk_size
-        self.batch_size = batch_size
-    model_id: str | None
-    revision: str | None
-    chunk_size: int | None
-    batch_size: int | None
-
 class TextEmbedConfig:
     """
     Represents the configuration for the Text Embedding model.
@@ -216,58 +160,6 @@ class TextEmbedConfig:
         self.batch_size = batch_size
     chunk_size: int | None
     batch_size: int | None
-
-class JinaConfig:
-    """Represents the configuration for the Jina model.
-
-    Attributes:
-        model_id: The ID of the Jina model from huggingface.
-        revision: The revision of the Jina model.
-        chunk_size: The chunk size for the Jina model.
-        batch_size: The batch size for processing the embeddings. Default is 32. Based on the memory, you can increase or decrease the batch size.
-    """
-
-    def __init__(
-        self,
-        model_id: str | None = None,
-        revision: str | None = None,
-        chunk_size: int | None = None,
-        batch_size: int | None = None,
-    ):
-        self.model_id = model_id
-        self.revision = revision
-        self.chunk_size = chunk_size
-        self.batch_size = batch_size
-    model_id: str | None = None
-    revision: str | None = None
-    chunk_size: int | None = None
-    batch_size: int | None = None
-
-class CloudConfig:
-    """Represents the configuration for the OpenAI model.
-
-    Attributes:
-        provider: The provider of the Cloud model. Default is OpenAI.
-        model: The name of the OpenAI model.
-        api_key: The API key for the OpenAI model.
-        chunk_size: The chunk size for the OpenAI model.
-    """
-
-    def __init__(
-        self,
-        provider: str | None = None,
-        model: str | None = None,
-        api_key: str | None = None,
-        chunk_size: int | None = None,
-    ):
-        self.provider = provider
-        self.model = model
-        self.api_key = api_key
-        self.chunk_size = chunk_size
-    provider: str | None
-    model: str | None
-    api_key: str | None
-    chunk_size: int | None
 
 class ClipConfig:
     """Represents the configuration for the Clip model.
@@ -322,10 +214,6 @@ class AudioDecoderConfig:
     quantized: bool | None
 
 class EmbeddingModel:
-    def __init__(self, model_id: str, revision: str | None = None):
-        self.model_id = model_id
-        self.revision = revision
-
     def from_pretrained_local(
         self, model: WhichModel, model_id: str, revision: str | None = None
     ): ...
