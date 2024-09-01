19d5119808e3b667de84a527a9a948ddc3806555
Akshay Ballal <arballal95@gmail.com>
2024-09-02 02:51:10 +0530

    fixed pyo3
    Former-commit-id: 0f7bc8852a4f90701f49eb7e9ea4f72737403f9c

--- rust/examples/bert.rs
+++ rust/examples/bert.rs
@@ -1,25 +1,16 @@
 use embed_anything::config::TextEmbedConfig;
 use embed_anything::embeddings::embed::{EmbedData, Embeder};
-use embed_anything::{embed_directory, embed_file};
+use embed_anything::{ embed_directory_stream, embed_file};
+use std::sync::Arc;
 use std::{path::PathBuf, time::Instant};
 
 #[tokio::main]
 async fn main() {
     let now = Instant::now();
 
-    let model = Embeder::from_pretrained_hf("bert", "sentence-transformers/all-MiniLM-L6-v2", None)
+    let model = Embeder::from_pretrained_hf("bert", "sentence-transformers/all-MiniLM-L12-v2", None)
         .unwrap();
     let config = TextEmbedConfig::new(Some(256), Some(32));
-    let _out = embed_directory(
-        PathBuf::from("test_files"),
-        &model,
-        None,
-        // Some(vec!["pdf".to_string()]),
-        Some(&config),
-        None::<fn(Vec<EmbedData>)>,
-    )
-    .await
-    .unwrap();
 
     let out = embed_file(
         "test_files/test.pdf",
@@ -29,8 +20,22 @@ async fn main() {
     )
     .await.unwrap().unwrap();
 
+    let model = Arc::new(model);
+
+    let _out = embed_directory_stream(
+        PathBuf::from("test_files"),
+        &model,
+        None,
+        // Some(vec!["txt".to_string()]),
+        Some(&config),
+        None::<Arc<fn(Vec<EmbedData>)>>,
+    )
+    .await
+    .unwrap().unwrap();
+
+
     println!("\nEmbedded Text from PDF:");
-    for (index, embed_data) in out.iter().enumerate() {
+    for (index, embed_data) in _out.iter().enumerate() {
         println!("Chunk {}:", index + 1);
         println!("Text: {}", embed_data.text.clone().unwrap());
         println!("Embedding length: {}", embed_data.embedding.len());
