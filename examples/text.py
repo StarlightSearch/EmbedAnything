c8e90319521ff07bfce370c4dad07de9fb4a7529
Akshay Ballal <arballal95@gmail.com>
2024-10-08 15:34:37 +0200

    handle empty chunks

--- examples/text.py
+++ examples/text.py
@@ -6,16 +6,16 @@ import os
 from time import time
 
 
-# model = EmbeddingModel.from_pretrained_hf(
-#     WhichModel.Bert, model_id="sentence-transformers/all-MiniLM-L12-v2"
-# )
+model = EmbeddingModel.from_pretrained_hf(
+    WhichModel.Bert, model_id="sentence-transformers/all-MiniLM-L12-v2"
+)
 
 # # with semantic encoder
 # # semantic_encoder = EmbeddingModel.from_pretrained_hf(WhichModel.Jina, model_id = "jinaai/jina-embeddings-v2-small-en")
 # # config = TextEmbedConfig(chunk_size=256, batch_size=32, splitting_strategy = "semantic", semantic_encoder=semantic_encoder)
 
 # # without semantic encoder
-# config = TextEmbedConfig(chunk_size=256, batch_size=32, splitting_strategy = "sentence")
+config = TextEmbedConfig(chunk_size=256, batch_size=32, splitting_strategy = "sentence")
 
 start = time()
 # data = embed_anything.embed_file("test_files/bank.txt", embeder=model, config=config)
@@ -24,9 +24,9 @@ start = time()
 #     print(d.text)
 #     print("---"*20)
 
-# data: list[EmbedData] = embed_anything.embed_directory(
-#     "test_files", embeder=model, config=config
-# )
+data: list[EmbedData] = embed_anything.embed_directory(
+    " test_files", embeder=model, config=config
+)
 
 config = TextEmbedConfig(chunk_size=256, batch_size=32, splitting_strategy = "sentence", sparse_embeddings=True)
 model = EmbeddingModel.from_pretrained_hf(WhichModel.Bert, model_id="prithivida/Splade_PP_en_v1")
