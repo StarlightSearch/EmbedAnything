c51a212811ccf1d4d6de89bdf77376824c2f0dfd
Akshay Ballal <arballal95@gmail.com>
2024-10-09 23:39:38 +0200

    ort
    Former-commit-id: f23bff513bb5eb1c114b05559494d46589b571d2

--- rust/examples/bert_ort.rs
+++ rust/examples/bert_ort.rs
@@ -8,18 +8,20 @@ use std::{path::PathBuf, time::Instant};
 #[tokio::main]
 async fn main() {
     let model = Arc::new(
-        Embeder::from_pretrained_hf("jina", "jinaai/jina-embeddings-v2-small-en", None).unwrap(),
+        Embeder::from_pretrained_ort("bert", "BAAI/bge-small-en-v1.5", None).unwrap(),
     );
     let config = TextEmbedConfig::new(
         Some(256),
         Some(32),
-        Some(32),
-        Some(SplittingStrategy::Semantic),
+        Some(1),
+        Some(SplittingStrategy::Sentence),
         Some(model.clone()),
     );
 
+    let now = Instant::now();
+
     let _out = embed_file(
-        "test_files/test.pdf",
+        "bench/attention.pdf",
         &model,
         Some(&config),
         None::<fn(Vec<EmbedData>)>,
@@ -28,12 +30,8 @@ async fn main() {
     .unwrap()
     .unwrap();
 
-    // let chunks = out.into_iter().map(|x| x.text.unwrap()).collect::<Vec<String>>();
-
-    // for chunk in chunks {
-    //     println!("{}", chunk);
-    //     println!("---");
-    // }
+    let elapsed_time = now.elapsed();
+    println!("Elapsed Time: {}", elapsed_time.as_secs_f32());
 
     let now = Instant::now();
 
@@ -53,14 +51,4 @@ async fn main() {
     let elapsed_time = now.elapsed();
     println!("Elapsed Time: {}", elapsed_time.as_secs_f32());
 
-    // println!("\nEmbedded Text from PDF:");
-
-    // println!("\nEmbedded Text from PDF:");
-    // for (index, embed_data) in _out.iter().enumerate() {
-    //     println!("Chunk {}:", index + 1);
-    //     println!("Text: {}", embed_data.text.clone().unwrap());
-    //     println!("Embedding length: {}", embed_data.embedding.len());
-    //     println!("Metadata: {:?}", embed_data.metadata);
-    //     println!("---");
-    // }
 }
