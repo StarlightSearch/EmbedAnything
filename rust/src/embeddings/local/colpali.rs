87c1ac9ef33858a46cafd62909be9f03343ce735
Akshay Ballal <arballal95@gmail.com>
2024-10-12 15:46:19 +0200

    colpali works
    Former-commit-id: 3cb012fdbeaa653dcb6744f53b5b8ff605ca28bb [formerly 4c4ae63257ee4149962d24c357bdeae1b01d7d61] [formerly 4e76a2ba62c77cc07e2cdf4c40297cbfa4732def [formerly dfa4023c8ea5e3b640b8369b70bea5980b5b5ddc]]
    Former-commit-id: 4e76a2ba62c77cc07e2cdf4c40297cbfa4732def
    Former-commit-id: da26258ef5208108edd93f02547ebc7aabc1d1ff

--- rust/src/embeddings/local/colpali.rs
+++ rust/src/embeddings/local/colpali.rs
@@ -12,13 +12,14 @@ pub struct ColPaliEmbedder {
     pub model: RwLock<Model>,
     pub tokenizer: Tokenizer,
     pub config: paligemma::Config,
+    dtype: DType,
     dummy_input: Tensor,
 }
 
 impl ColPaliEmbedder {
     pub fn new(model_id: &str, revision: Option<&str>) -> Result<Self, anyhow::Error> {
         let api = hf_hub::api::sync::Api::new()?;
-        let api = match revision {
+        let repo: hf_hub::api::sync::ApiRepo = match revision {
             Some(rev) => api.repo(hf_hub::Repo::with_revision(
                 model_id.to_string(),
                 hf_hub::RepoType::Model,
@@ -30,27 +31,22 @@ impl ColPaliEmbedder {
             )),
         };
 
-        let (config_filename, tokenizer_filename, weights_filename) = {
-            let config = api.get("config.json")?;
-            let tokenizer = api.get("tokenizer.json")?;
-            let weights = match api.get("model.safetensors") {
-                Ok(safetensors) => safetensors,
-                Err(_) => match api.get("pytorch_model.bin") {
-                    Ok(pytorch_model) => pytorch_model,
-                    Err(e) => {
-                        return Err(anyhow::Error::msg(format!(
-                            "Model weights not found. The weights should either be a `model.safetensors` or `pytorch_model.bin` file.  Error: {}",
-                            e
-                        )));
-                    }
-                },
-            };
+        let tokenizer_api = api.repo(hf_hub::Repo::new(
+            "vidore/colpali".to_string(),
+            hf_hub::RepoType::Model,
+        ));
 
-            (config, tokenizer, weights)
+        let (tokenizer_filename, weights_filename) = {
+            let tokenizer = tokenizer_api.get("tokenizer.json")?;
+            let weights = hub_load_safetensors(&repo, "model.safetensors.index.json")?;
+
+            (tokenizer, weights)
         };
 
-        let config = std::fs::read_to_string(config_filename)?;
-        let config: paligemma::Config = serde_json::from_str(&config)?;
+        // let config = std::fs::read_to_string(config_filename)?;
+        // let config: paligemma::Config = serde_json::from_str(&config)?;
+        let config: paligemma::Config = paligemma::Config::paligemma_3b_448();
+
         let mut tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
 
         let pp = PaddingParams {
@@ -76,14 +72,7 @@ impl ColPaliEmbedder {
             DType::F32
         };
 
-        let vb = if weights_filename.ends_with("model.safetensors") {
-            unsafe {
-                VarBuilder::from_mmaped_safetensors(&[weights_filename], dtype, &device).unwrap()
-            }
-        } else {
-            println!("Loading weights from pytorch_model.bin");
-            VarBuilder::from_pth(&weights_filename, dtype, &device).unwrap()
-        };
+        let vb = unsafe { VarBuilder::from_mmaped_safetensors(&weights_filename, dtype, &device)? };
 
         let model = Model::new(&config, vb)?;
         let dummy_prompt: &str = "Describe the image";
@@ -94,11 +83,12 @@ impl ColPaliEmbedder {
             model: RwLock::new(model),
             tokenizer,
             config,
+            dtype,
             dummy_input,
         })
     }
 
-    fn load_image<T: AsRef<std::path::Path>>(
+    pub fn load_image<T: AsRef<std::path::Path>>(
         &self,
         path: T,
         image_size: usize,
@@ -142,6 +132,38 @@ impl ColPaliEmbedder {
 
         Ok(images)
     }
+
+    pub fn embed(
+        &self,
+        text_batch: &[String],
+        batch_size: Option<usize>,
+    ) -> Result<Vec<EmbeddingResult>, anyhow::Error> {
+        let mut encodings = Vec::new();
+        for mini_text_batch in text_batch.chunks(batch_size.unwrap_or(32)) {
+            let input_ids = tokenize_batch(
+                &self.tokenizer,
+                mini_text_batch
+                    .iter()
+                    .map(|s| s.as_str())
+                    .collect::<Vec<_>>(),
+                &Device::cuda_if_available(0).unwrap_or(Device::Cpu),
+            )?;
+            let batch_encodings = self
+                .model
+                .write()
+                .unwrap()
+                .forward_text(&input_ids)?
+                .to_dtype(DType::F32)?;
+
+            encodings.extend(
+                batch_encodings
+                    .to_vec3::<f32>()?
+                    .iter()
+                    .map(|x| EmbeddingResult::Sparse(x.to_vec())),
+            );
+        }
+        Ok(encodings)
+    }
 }
 
 impl EmbedImage for ColPaliEmbedder {
@@ -152,12 +174,14 @@ impl EmbedImage for ColPaliEmbedder {
     ) -> anyhow::Result<EmbedData> {
         let pixel_values = self
             .load_image(image_path, self.config.vision_config.image_size)?
-            .unsqueeze(0)?;
+            .unsqueeze(0)?
+            .to_dtype(self.dtype)?;
         let encoding = self
             .model
             .write()
             .unwrap()
             .forward_images(&pixel_values, &self.dummy_input)?
+            .to_dtype(DType::F32)?
             .to_vec3::<f32>()?
             .iter()
             .map(|x| EmbeddingResult::Sparse(x.to_vec()))
@@ -170,12 +194,15 @@ impl EmbedImage for ColPaliEmbedder {
         &self,
         image_paths: &[T],
     ) -> anyhow::Result<Vec<EmbedData>> {
-        let pixel_values = self.load_images(image_paths, self.config.vision_config.image_size)?;
+        let pixel_values = self
+            .load_images(image_paths, self.config.vision_config.image_size)?
+            .to_dtype(self.dtype)?;
         let encodings = self
             .model
             .write()
             .unwrap()
             .forward_images(&pixel_values, &self.dummy_input)?
+            .to_dtype(DType::F32)?
             .to_vec3::<f32>()?;
 
         Ok(encodings
@@ -200,4 +227,30 @@ fn tokenize_batch(
         .collect::<candle_core::Result<Vec<_>>>()?;
 
     Ok(Tensor::stack(&token_ids, 0)?)
+}
+
+pub fn hub_load_safetensors(
+    repo: &hf_hub::api::sync::ApiRepo,
+    json_file: &str,
+) -> Result<Vec<std::path::PathBuf>, E> {
+    let json_file = repo.get(json_file).map_err(candle_core::Error::wrap)?;
+    let json_file = std::fs::File::open(json_file)?;
+    let json: serde_json::Value =
+        serde_json::from_reader(&json_file).map_err(candle_core::Error::wrap)?;
+    let weight_map = match json.get("weight_map") {
+        None => anyhow::bail!("no weight map in {json_file:?}"),
+        Some(serde_json::Value::Object(map)) => map,
+        Some(_) => anyhow::bail!("weight map in {json_file:?} is not a map"),
+    };
+    let mut safetensors_files = std::collections::HashSet::new();
+    for value in weight_map.values() {
+        if let Some(file) = value.as_str() {
+            safetensors_files.insert(file.to_string());
+        }
+    }
+    let safetensors_files = safetensors_files
+        .iter()
+        .map(|v| repo.get(v).map_err(candle_core::Error::wrap))
+        .collect::<Result<Vec<_>, _>>()?;
+    Ok(safetensors_files)
 }
